662afa6d17c8240e0afaf8a4a4210df3bd97755b
LAPTOP-T24CVE1L\91709 <pothapalavinodh@gmail.com>
2021-08-27 11:53:29 -0700

    Addition of 2 numbers Sorting and count

--- Hello.py
+++ Hello.py
@@ -1,5 +1,10 @@
-import math
+def func(y):
+    return y % 2
+
+
 print("Hello world");
-x = 2
-y = 3
-print(x+y)
+x = [2, 5, 6, 7, 9, 9, 1, -1]
+print(x.count(9))
+x.sort()
+print(sorted(x))
+print(sorted(x, key=func))
